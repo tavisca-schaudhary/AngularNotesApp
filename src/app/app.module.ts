c41a3e08b3c62a95440cf153023f1f5d3c7dd9ed
Shweta Chaudhary <schaudhary@affinionds.com>
2021-01-29 03:26:14 +0530

    Added changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { IdeaboardComponent } from './ideaboard/ideaboard.component';
+
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './store/reducer/note.reducer';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    IdeaboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      note: reducer,
+    }),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
